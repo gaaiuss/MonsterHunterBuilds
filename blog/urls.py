3feab435bfc39fc20c6f796101429dcbe08f67d9
gaaiuss <caio.gui.castro@gmail.com>
2025-10-23 14:18:32 -0300

    dinamic urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.blog),
-    path('example/', views.example),
+    path('', views.blog, name='home'),
+    path('example/', views.example, name='example'),
 ]
