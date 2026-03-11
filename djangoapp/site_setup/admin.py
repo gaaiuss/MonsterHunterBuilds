4628c7afdab0011f7dbce9a502b9848d2c7a9b97
gaaiuss <caio.gui.castro@gmail.com>
2026-03-11 13:38:50 -0300

    add: type annotations

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
 from site_setup.models import MenuLink, SiteSetup
 
 
 @admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
+class MenuLinkAdmin(admin.ModelAdmin):  # type: ignore
     list_display = "id", "text", "url_or_path"
     list_display_links = "id", "text", "url_or_path"
     search_fields = "id", "text", "url_or_path"
 
 
 @admin.register(SiteSetup)
-class SiteSetupAdmin(admin.ModelAdmin):
+class SiteSetupAdmin(admin.ModelAdmin):  # type: ignore
     list_display = "title", "description"
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        print(request)
         return not SiteSetup.objects.exists()
