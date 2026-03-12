91c0a708df5976d2bfbe72f8149419ed7249d5bc
gaaiuss <caio.gui.castro@gmail.com>
2026-03-12 11:28:05 -0300

    fix: menu links inside sitesetup

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -4,16 +4,15 @@ from django.http import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):  # type: ignore
-    list_display = "id", "text", "url_or_path"
-    list_display_links = "id", "text", "url_or_path"
-    search_fields = "id", "text", "url_or_path"
+class MenuLinksInLine(admin.TabularInline):  # type: ignore
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):  # type: ignore
     list_display = "title", "description"
+    inlines = (MenuLinksInLine,)
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         print(request)
