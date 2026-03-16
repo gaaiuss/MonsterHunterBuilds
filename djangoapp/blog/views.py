4bd3430249f5d2d69943aad5a7592115163a46e3
gaaiuss <caio.gui.castro@gmail.com>
2026-03-16 13:25:09 -0300

    add: necessary templates for blog

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "blog/pages/index.html")
+
+
+def page(request: HttpRequest) -> HttpResponse:
+    return render(request, "blog/pages/page.html")
+
+
+def post(request: HttpRequest) -> HttpResponse:
+    return render(request, "blog/pages/post.html")
