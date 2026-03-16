5b5dcd9bcb5a8e7003dbc605faa714ff1f00b078
gaaiuss <caio.gui.castro@gmail.com>
2026-03-16 14:06:59 -0300

    add: OM blog template (HTML and CSS)

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,17 @@
+from django.core.paginator import Paginator
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+posts = list(range(1000))
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/pages/index.html")
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "blog/pages/index.html", {"page_obj": page_obj})
 
 
 def page(request: HttpRequest) -> HttpResponse:
