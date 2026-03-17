e27a0edb6a94472672baefa9f93448e676aee670
gaaiuss <caio.gui.castro@gmail.com>
2026-03-17 10:49:20 -0300

    add: initial page model

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Category, Tag
+from blog.models import Category, Page, Tag
 
 
 @admin.register(Tag)
@@ -37,3 +37,22 @@ class CategoryAdmin(admin.ModelAdmin):  # type: ignore
     list_per_page = 10
     ordering = ("-id",)
     prepopulated_fields = {"slug": ("name",)}  # noqa: RUF012
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = (
+        "id",
+        "title",
+        "is_published",
+    )
+    list_display_links = ("title",)
+    search_fields = (
+        "id",
+        "title",
+        "slug",
+        "content",
+    )
+    list_per_page = 10
+    ordering = ("-id",)
+    prepopulated_fields = {"slug": ("title",)}  # noqa: RUF012
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -17,6 +17,9 @@ class Tag(models.Model):
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)  # type: ignore
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -32,3 +35,25 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)  # type: ignore
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=50
+    )
+    is_published = models.BooleanField(
+        default=False, help_text="Mark this if you want to share your page publicly."
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs) -> None:  # type: ignore # noqa: ANN002, ANN003
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+        return super().save(*args, **kwargs)  # type: ignore
+
+    def __str__(self) -> str:
+        return self.title
