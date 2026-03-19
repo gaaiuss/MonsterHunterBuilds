374d113138b61c90318fd50abdbe38e75df0afbc
gaaiuss <caio.gui.castro@gmail.com>
2026-03-19 14:24:10 -0300

    fix: post urls, views

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -3,11 +3,14 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
-posts = list(range(1000))
+from blog.models import Post
+
+PER_PAGE = 9
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    paginator = Paginator(posts, 9)
+    posts = Post.objects.filter(is_published=True).order_by("-pk")
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
