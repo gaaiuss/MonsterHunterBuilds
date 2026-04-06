2ea4b19a054801c3311df17e666a5e7f98fd261c
gaaiuss <caio.gui.castro@gmail.com>
2026-04-06 13:55:26 -0300

    add: base security

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DOT_ENV
+load_dotenv(BASE_DIR.parent / "dotenv_files" / ".env", override=True)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
